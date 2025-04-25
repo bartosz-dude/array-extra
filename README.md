0ee4d6ea9557b6f348c764faedaa20c505d1f3f3
bartosz-dude <bartoszdudzinskidev@gmail.com>
2025-04-25 20:48:29 +0200

    more stuff

--- README.md
+++ README.md
@@ -1,5 +1,93 @@
 # array-extra
 
+A mostly drop in replacement for an array with extra features.
+
+## Usage
+
+`ArrayExtra` class extends the `Array` class, so it can be used everywhere that an array can be used. It implements every method that `Array` has while returning an `ArrayExtra` instead.
+
+To create a new array using `ArrayExtra` you use the static method `of`.
+
+```typescript
+const arr = ArrayExtra.of(1, 2, 3)
+```
+
+## Typing
+
+Unlike `Array` where generic is the array's item type, `ArrayExtra`'s generic is the whole array type which allows to make tuples using it, which is the default.
+
+```typescript
+const arr1 = [1, 2, 3] // type is number[]
+const arr2 = [1, 2, 3] as [number, number, number] // type is [number, number, number]
+const arr3 = Array.of(1, 2, 3) // type is number[]
+
+const arr4 = ArrayExtra.of(1, 2, 3) // type is ArrayExtra<[number, number, number]>
+const arr41 = arr4.toArray() // type is [number, number, number]
+const arr5 = arrayExtra.of<number[]>(1, 2, 3) // type is ArrayExtra<number[]>
+const arr51 = arr5.toArray() // type is number[]
+
+const arr6 = arrayExtra.of(1, "a", 2) // type is ArrayExtra<[number, string, number]>
+const arr7 = arrayExtra.of<[number, "a", number]>(1, "a", 2) // type is ArrayExtra<[number, "a", number]>
+```
+
+## The extra methods
+<!-- ! Check if Array.isArray actually returns true -->
+Checks if the object is an instance of `ArrayExtra`, also `Array.isArray` returns `true` for an instance of `ArrayExtra`.
+
+```typescript
+ArrayExtra.isArrayExtra()
+```
+
+Returns a copy of the array without item at the provided index, it can be negative to count from the end and when it goes beyound array's length, then it just returns a copy of the array.
+
+```typescript
+arr.without()
+```
+
+Returns a copy of the `ArrayExtra` but as an instance of `Array`.
+
+```typescript
+arr.toArray()
+```
+
+Returns a copy of the array with effect of `.unshift()`, not modifing the original array.
+
+```typescript
+arr.toUshifted()
+```
+
+Returns a copy of the array with effect of `.push()`, not modifing the original array.
+
+```typescript
+arr.toPushed()
+```
+
+Returns a copy of the array with effect of `.unshift()`, but before the provided index instead of the beggining of the array.
+
+```typescript
+arr.toUnshiftedIndex()
+```
+
+Returns a copy of the array with effect of `.push()`, but after the provided index instead of the end of the array.
+
+```typescript
+arr.toPushedIndex()
+```
+
+## Known issues
+
+These methods have incorrect typing:
+
+- `flat`
+- `faltMap`
+
+Thse methods don't return `ArrayExtra`:
+
+- `reduce`
+- `reduceRight`
+- `flat`
+- `flatMap`
+
 ## Support
 
 If you like the package, you can support me by buying me a tea.
--- index.ts
+++ index.ts
@@ -1,9 +1,35 @@
-export class ArrayExtra<T extends any[]> extends Array {
-	constructor(...items: T) {
-		super()
-		for (const item of items) {
-			this.push(item)
+type InnerArray<T extends Array<unknown>> = T[number][]
+// type FlatArrayExtra<Arr extends ArrayExtra<unknown[]>, Depth extends number> = {
+//     done: Arr;
+//     recur: Arr extends ArrayExtra<Array<infer I>> ? FlatArrayExtra<InnerArr, [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20][Depth]> : Arr;
+// }[ Depth extends -1 ? "done" : "recur" ]
+
+export class ArrayExtra<T extends Array<unknown>> extends Array<T[number]> {
+	static of<T extends Array<unknown>>(...items: T) {
+		const arr = new ArrayExtra<T>(items.length)
+
+		for (let i = 0; i < items.length; i++) {
+			arr[i] = items[i]
 		}
+
+		return arr
+	}
+
+	static isArrayExtra(arg: any) {
+		return arg instanceof ArrayExtra
+	}
+
+	static from<T>(iterable: Iterable<T> | ArrayLike<T>): ArrayExtra<T[]> {
+		return ArrayExtra.of(...super.from(iterable))
+	}
+
+	static async fromAsync(
+		arrayLike:
+			| AsyncIterable<unknown>
+			| Iterable<unknown>
+			| ArrayLike<unknown>
+	): Promise<ArrayExtra<unknown[]>> {
+		return ArrayExtra.of(...(await super.fromAsync(arrayLike)))
 	}
 
 	#normalizeIndex(index: number): number | undefined {
@@ -24,10 +50,141 @@ export class ArrayExtra<T extends any[]> extends Array {
 		return index
 	}
 
+	concat(
+		...items: (T[number] | ConcatArray<T[number]>)[]
+	): ArrayExtra<InnerArray<T>> {
+		return ArrayExtra.of(...super.concat(...items))
+	}
+
+	filter<S extends T[number]>(
+		predicate: (
+			value: T[number],
+			index: number,
+			array: InnerArray<T>
+		) => value is S,
+		thisArg?: any
+	): ArrayExtra<S[]> {
+		return ArrayExtra.of(...super.filter(predicate, thisArg))
+	}
+
+	map<U>(
+		callbackfn: (
+			value: T[number],
+			index: number,
+			array: InnerArray<T>
+		) => U,
+		thisArg?: any
+	): ArrayExtra<U[]> {
+		return ArrayExtra.of(...super.map(callbackfn, thisArg))
+	}
+
+	// // @ts-expect-error
+	// reduce<U>(
+	// 	callbackfn: (
+	// 		previousValue: U,
+	// 		currentValue: T[number],
+	// 		currentIndex: number,
+	// 		array: InnerArray<T>
+	// 	) => U,
+	// 	initialValue: U
+	// ): U extends unknown[] ? ArrayExtra<U> : U {
+	// 	const red = super.reduce(callbackfn, initialValue)
+
+	// 	if (Array.isArray(red)) {
+	// 		// @ts-expect-error
+	// 		return ArrayExtra.from(red)
+	// 	}
+
+	// 	// @ts-expect-error
+	// 	return red
+	// }
+
+	// // @ts-expect-error
+	// reduceRight<U>(
+	// 	callbackfn: (
+	// 		previousValue: U,
+	// 		currentValue: T[number],
+	// 		currentIndex: number,
+	// 		array: InnerArray<T>
+	// 	) => U,
+	// 	initialValue: U
+	// ): U extends unknown[] ? ArrayExtra<U> : U {
+	// 	const red = super.reduceRight(callbackfn, initialValue)
+
+	// 	if (Array.isArray(red)) {
+	// 		// @ts-expect-error
+	// 		return ArrayExtra.from(red)
+	// 	}
+
+	// 	// @ts-expect-error
+	// 	return red
+	// }
+
+	reverse(): ArrayExtra<T> {
+		super.reverse()
+		return this
+	}
+
+	toReversed(): ArrayExtra<InnerArray<T>> {
+		const arr = this.concat()
+		arr.reverse()
+		return arr
+	}
+
+	toSorted(
+		compareFn?: ((a: T[number], b: T[number]) => number) | undefined
+	): ArrayExtra<InnerArray<T>> {
+		const arr = this.concat()
+		arr.sort(compareFn)
+		return arr
+	}
+
+	toSpliced(start: number, deleteCount?: number): ArrayExtra<InnerArray<T>>
+	toSpliced(
+		start: number,
+		deleteCount: number,
+		...items: unknown[]
+	): ArrayExtra<InnerArray<T>>
+	toSpliced(
+		start: number,
+		deleteCount: number,
+		...items: unknown[]
+	): ArrayExtra<InnerArray<T>> {
+		const arr = this.concat()
+		arr.splice(start, deleteCount, ...items)
+		return arr
+	}
+
+	splice(start: number, deleteCount?: number): ArrayExtra<InnerArray<T>>
+	splice(
+		start: number,
+		deleteCount: number,
+		...items: unknown[]
+	): ArrayExtra<InnerArray<T>>
+	splice(
+		start: number,
+		deleteCount: number,
+		...items: unknown[]
+	): ArrayExtra<InnerArray<T>> {
+		return ArrayExtra.from(super.splice(start, deleteCount, ...items))
+	}
+
+	with(index: number, value: T[number]): ArrayExtra<InnerArray<T>> {
+		return ArrayExtra.from(super.with(index, value))
+	}
+
+	// flat<A, D extends number = 1>(
+	// 	this: A,
+	// 	depth?: D | undefined
+	// ): ArrayExtra<FlatArray<A, D>[]> {
+	// 	// @ts-expect-error
+	// 	return ArrayExtra.from(super.flat(depth))
+	// }
+
 	/**
 	 * Copies an array, then removes the value at the provided index. If the index is negative, then it removes from the end of the array. If index is beyound the array, then returns just a copy of the array.
 	 * @param index The index of the value to remove. If the index is negative, then it removes from the end of the array.
-	 * @returns  The copied array with the removed value.
+	 * @returns The copied array with the removed value.
 	 */
 	without(index: number) {
 		const i = this.#normalizeIndex(index)
@@ -41,8 +198,94 @@ export class ArrayExtra<T extends any[]> extends Array {
 
 		return front.concat(end)
 	}
-}
 
-const a = new ArrayExtra<(number | string)[]>(1, "a", "b")
-const b = new ArrayExtra()
-a.push("a")
+	/**
+	 * Copies an array, then inserts new elements before element at the provided index, and returns the new array.
+	 * @param index The index of the value to insert before. If the index is negative, then it goes from the end of the array.
+	 * @param items Elements to insert before element at the provided index.
+	 * @returns The copied array with the inserted values.
+	 */
+	toUnshiftedIndex(index: number, ...items: InnerArray<T>) {
+		const i = this.#normalizeIndex(index)
+
+		if (i === undefined) {
+			return
+		}
+
+		if (i === 0) {
+			this.unshift(...items)
+		}
+
+		const front = this.slice(0, i)
+		const end = this.slice(i)
+		return ArrayExtra.of(...front, ...items, ...end)
+	}
+
+	/**
+	 * Copies an array, then inserts new elements after element at the provided index, and returns the new array.
+	 * @param index The index of the value to insert after. If the index is negative, then it goes from the end of the array.
+	 * @param items Elements to insert after element at the provided index.
+	 * @returns The copied array with the inserted values.
+	 */
+	toPushedIndex(index: number, ...items: InnerArray<T>) {
+		const i = this.#normalizeIndex(index)
+
+		if (i === undefined) {
+			return
+		}
+
+		if (i === 0) {
+			this.unshift(...items)
+		}
+
+		const front = this.slice(0, i)
+		const end = this.slice(i)
+		return ArrayExtra.of(...front, ...items, ...end)
+	}
+
+	/**
+	 * Copies an array, then inserts new elements at the start of an array, and returns the new array.
+	 * @param items Elements to insert at the start of the array.
+	 * @returns The copied array with the appended values.
+	 */
+	toUnshifted(...items: InnerArray<T>) {
+		const arr = this.concat()
+		arr.unshift(...items)
+		return arr
+	}
+
+	/**
+	 * Copies an array, then appends new elements to the end of an array, and returns the new array.
+	 * @param items New elements to add to the array.
+	 * @returns The copied array with the appended values.
+	 */
+	toPushed(...items: InnerArray<T>) {
+		const arr = this.concat()
+		arr.push(...items)
+		return arr
+	}
+
+	toShifted() {
+		const arr = this.concat()
+		arr.shift()
+		return arr
+	}
+
+	toPoped() {
+		const arr = this.concat()
+		arr.pop()
+		return arr
+	}
+
+	/**
+	 * Returns a copy of this ArrayExtra as an Array
+	 * @returns The copied array.
+	 */
+	toArray() {
+		return super.concat() as T
+	}
+
+	slice(start?: number, end?: number): ArrayExtra<InnerArray<T>> {
+		return ArrayExtra.from(super.slice(start, end))
+	}
+}
